bef10caed8f35cf7fc9d5314129a137f0d12d8e0
dkorolev <korwork87@yahoo.com>
2015-09-04 20:21:57 +0300

    skeleton of test application

---
+++ datapine-test-app/src/main/java/com/datapine/domain/User.java
@@ -0,0 +1,41 @@
+package com.datapine.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String email;
+
+	private String password;
+
+	public User(final String email) {
+		this.email = email;
+	}
+
+    public User() {
+    }
+
+    public Long getId() {
+		return id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+}
