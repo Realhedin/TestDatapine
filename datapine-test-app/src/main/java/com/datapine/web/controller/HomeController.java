0d22061a453cb23ed7535b56b2d4df98018b6f36
dkorolev <korwork87@yahoo.com>
2015-09-05 15:43:08 +0300

    added Log4j in pom dependency added logging for user attempts to HomeController

--- datapine-test-app/src/main/java/com/datapine/web/controller/HomeController.java
+++ datapine-test-app/src/main/java/com/datapine/web/controller/HomeController.java
@@ -4,6 +4,7 @@ import com.datapine.dao.UserDAO;
 import com.datapine.domain.User;
 import com.datapine.service.UserService;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -12,8 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+
+
 @Controller
 public class HomeController {
+
+    private final static Logger logger = Logger.getLogger(HomeController.class);
 
     @Autowired
     private UserService userService;
@@ -38,11 +43,14 @@ public class HomeController {
 
     @RequestMapping(value = "/tryLogin", method = RequestMethod.POST)
     public ModelAndView tryLogin(@ModelAttribute("user") User user, BindingResult result) {
+        logger.info("User: "+user.getEmail()+ " is trying to login into the system.");
         System.out.println("We are here");
         User loginUser = userDAO.findByEmail(user.getEmail());
         if (loginUser != null) {
+            logger.info("User was found and accepted!");
             return new ModelAndView("index");
         }
+        logger.info("User was not found. Redirected to login page");
         return new ModelAndView("login");
     }
 
